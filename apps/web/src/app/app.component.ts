6ef7a7d1da2b6d4a81df364d2b614f0ddfde1326
Eric Barb <eric.barb@gmail.com>
2019-12-08 22:18:25 -0500

    ng g @nrwl/angular:application web

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stylzz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'web';
+}

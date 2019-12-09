b4df7dbefe5652f6398c65dfffba26b5947f18f2
Eric Barb <eric.barb@gmail.com>
2019-12-09 10:55:46 -0500

    add UI library and todos component (#8)

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { UiModule } from '@stylzz/ui';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
